62796d46aea4ac5a19b0a21efb9fa363f5450f8e
DELL <DELL@ADMIN>
2020-07-14 08:55:21 +0530

    ClassNotFoundException And NOClassDefoundError Commit

---
+++ ClassNotFoundException_NoClassDefFoundError/src/com/alliswell/beans/Alien.java
@@ -0,0 +1,43 @@
+package com.alliswell.beans;
+
+public class Alien {
+	String name;
+	int id;
+	double salary;
+
+	public Alien(String name, int id, double salary) {
+		this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Alien [name=" + name + ", id=" + id + ", salary=" + salary + "]";
+	}
+
+}
---
+++ ClassNotFoundException_NoClassDefFoundError/src/com/alliswell/beans/AlienTesting.java
@@ -0,0 +1,18 @@
+package com.alliswell.beans;
+
+public class AlienTesting {
+	public static void main(String[] args) {
+		/*Alien alien = new Alien("abc", 1007, 14000);
+		System.out.println(alien);*/
+
+		// It will throws ClassNotFoundException...
+		/*try {
+			Class.forName("com.alliswell.beans.Alie");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}*/
+	}
+}
+
